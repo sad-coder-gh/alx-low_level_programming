73da5e9e8e188773bf89652bbd1f8a1a3c776f4c
usman <sadcodergh247@gmail.com>
2022-12-10 03:41:35 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     
- * 
+ * Return: Always 0
  */
 
 int main(void)
@@ -18,5 +12,5 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		return (0);
 }
